2c24563af21f539f9e120b310c9c0ea531a85d4b
Kerry Long <victory173451@gmail.com>
2022-03-31 18:56:34 -0400

    added format for dates

--- models/Thought.js
+++ models/Thought.js
@@ -1,56 +1,67 @@
-const { Schema, model, Types} = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const reactionSchema = new Schema({
   reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
   },
   reactionBody: {
-      type: String,
-      required: 'Reaction is Required',
-      max: 280
+    type: String,
+    required: 'Reaction is Required',
+    max: 280
   },
   userName: {
-      type: String,
-      required: 'User Name is Required'
+    type: String,
+    required: 'User Name is Required'
   },
   createdAt: {
-      type: Date,
-      default: Date.now,
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
   }
-});
+},
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
 
 const thoughtSchema = new Schema({
-    thoughtText: {
-        type: String,
-        required: 'Thought is Required',
-        min: 1,
-        max: 280
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-      },
-      userName: {
-        type: String,
-        unique: true,
-        required: 'User Name is Required',
-        trim: true
-    },
-    reactions: [reactionSchema]
+  thoughtText: {
+    type: String,
+    required: 'Thought is Required',
+    min: 1,
+    max: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
+  },
+  userName: {
+    type: String,
+    unique: true,
+    required: 'User Name is Required',
+    trim: true
+  },
+  reactions: [reactionSchema]
 },
-{
+  {
     toJSON: {
       virtuals: true,
+      getters: true
     },
     id: false
   }
 );
 
 // get total count of friends retrieval
-thoughtSchema.virtual('reactionCount').get(function() {
-    return this.reactions.length;
-  });
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
 
 const Thought = model('Thought', thoughtSchema);
 module.exports = Thought;
